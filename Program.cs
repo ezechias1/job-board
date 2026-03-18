c6693eb77a508ea6e023ad7426fe92c9b82d097c
ezechias <ezechiasmulamba@gmail.com>
2026-03-18 14:28:10 +0200

    Add Dockerfile and enable Swagger in production for deployment

--- Program.cs
+++ Program.cs
@@ -34,13 +34,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
